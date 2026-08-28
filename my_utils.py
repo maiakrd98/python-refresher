207512ef296ee7ead2a94d27d5588861c1afb626
Maia Richards-Dinger <maiakrd98@gmail.com>
2026-08-28 10:56:20 -0600

    implement get_column

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,12 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    results = []
+
+    with open(file_name) as file:
+        for line in file:
+
+            entries = line.split(sep=',')
+
+            if entries[query_column] == query_value:
+                results.append(entries[result_column])
+
+    return results
