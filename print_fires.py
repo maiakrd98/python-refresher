044f86ea112dc5e81f3242e8759b8ee103983a91
Maia Richards-Dinger <maiakrd98@gmail.com>
2026-09-03 10:43:57 -0600

    update get_column() with named argument

--- print_fires.py
+++ print_fires.py
@@ -4,5 +4,5 @@ country = 'Canada'
 country_column = 0
 fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = my_utils.get_column(file_name, country_column, country, fires_column)
+fires = my_utils.get_column(file_name, country_column, country, result_column=fires_column)
 print(fires)
