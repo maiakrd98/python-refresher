4ffeb01abe55d52a4558f69219a49d60e254fd81
Maia Richards-Dinger <maiakrd98@gmail.com>
2026-09-05 18:50:01 -0600

    fix python files to follow study guide

--- my_utils.py
+++ my_utils.py
@@ -1,4 +1,4 @@
-def get_column(file_name, query_column, query_value, result_column = 1):
+def get_column(file_name, query_column, query_value, result_column=1):
     results = []
 
     with open(file_name) as file:
--- print_fires.py
+++ print_fires.py
@@ -4,5 +4,6 @@ country = 'Canada'
 country_column = 0
 fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = my_utils.get_column(file_name, country_column, country, result_column=fires_column)
+fires = my_utils.get_column(file_name, country_column, country,
+                            result_column=fires_column)
 print(fires)
