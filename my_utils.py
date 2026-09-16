41fedfaea78edaacbc467f6a6cf2e8aca74793eb
Maia Richards-Dinger <maiakrd98@gmail.com>
2026-09-16 14:56:12 -0600

    make get_columns return ints and catch exceptions

--- my_utils.py
+++ my_utils.py
@@ -1,12 +1,43 @@
+import sys
+
+
 def get_column(file_name, query_column, query_value, result_column=1):
     results = []
 
-    with open(file_name) as file:
-        for line in file:
+    try:
+        file = open(file_name, 'r')
+    except FileNotFoundError:
+        print('Could not find ' + file_name)
+        sys.exit(1)
+    except PermissionError:
+        print('Could not open ' + file_name)
+        sys.exit(1)
 
-            entries = line.split(sep=',')
+    for line in file:
 
-            if entries[query_column] == query_value:
-                results.append(entries[result_column])
+        entries = line.split(sep=',')
+
+        try:
+            query_entry = entries[query_column]
+        except IndexError:
+            print('query_column index is out of bounds')
+            sys.exit(1)
+
+        if query_entry == query_value:
+
+            try:
+                result_entry = entries[result_column]
+            except IndexError:
+                print('result_column index is out of bounds')
+                sys.exit(1)
+
+            try:
+                result_float = float(result_entry)
+            except ValueError:
+                print("Could not convert entry in result_column ('" +
+                      result_entry + "') to float")
+                sys.exit()
+
+            results.append(round(result_float))
 
     return results
