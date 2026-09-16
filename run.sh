0f6ef66ce94ea61ebfd944a7ed2599cc7e2a91ab
Maia Richards-Dinger <maiakrd98@gmail.com>
2026-09-16 15:03:33 -0600

    update run.sh with more examples

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python print_fires.py
+python print_fires.py --file_name 'Agrofood_co2_emission.csv' --country 'Canada' --country_column 0  --fires_column 3
+python print_fires.py --file_name 'Agrofood_co2_emision.csv' --country 'Canada' --country_column 0  --fires_column 3
+python print_fires.py --file_name 'Agrofood_co2_emission.csv' --country 'Canada' --country_column 0  --fires_column 31
