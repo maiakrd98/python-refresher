e2bd952eeb7d823dd83778ea1fdbbd7f06b148f0
Maia Richards-Dinger <maiakrd98@gmail.com>
2026-09-16 13:33:21 -0600

    update print_fires.py to use argparse

--- print_fires.py
+++ print_fires.py
@@ -1,9 +1,32 @@
 import my_utils
+import argparse
 
-country = 'Canada'
-country_column = 0
-fires_column = 3
-file_name = 'Agrofood_co2_emission.csv'
-fires = my_utils.get_column(file_name, country_column, country,
-                            result_column=fires_column)
+parser = argparse.ArgumentParser(
+                    prog='print_fires',
+                    description='Prints a given column from a given country')
+
+parser.add_argument('--country',
+                    type=str,
+                    help='The country you are interested in',
+                    required=True)
+
+parser.add_argument('--country_column',
+                    type=int,
+                    help='The column in which countries are listed',
+                    required=True)
+
+parser.add_argument('--fires_column',
+                    type=int,
+                    help='The column in which wildfire emmissions are listed',
+                    required=True)
+
+parser.add_argument('--file_name',
+                    type=str,
+                    help='The name of the file',
+                    required=True)
+
+args = parser.parse_args()
+
+fires = my_utils.get_column(args.file_name, args.country_column, args.country,
+                            result_column=args.fires_column)
 print(fires)
