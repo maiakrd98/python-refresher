59f06f20394c22a7169b75ca778f48003001bf68
Maia Richards-Dinger <maiakrd98@gmail.com>
2026-09-16 13:40:53 -0600

    update print_fires.py to use main function

--- print_fires.py
+++ print_fires.py
@@ -1,32 +1,38 @@
 import my_utils
 import argparse
 
-parser = argparse.ArgumentParser(
-                    prog='print_fires',
-                    description='Prints a given column from a given country')
 
-parser.add_argument('--country',
-                    type=str,
-                    help='The country you are interested in',
-                    required=True)
+def main():
+    parser = argparse.ArgumentParser(
+                        prog='print_fires',
+                        description='Prints given column from given country')
 
-parser.add_argument('--country_column',
-                    type=int,
-                    help='The column in which countries are listed',
-                    required=True)
+    parser.add_argument('--country',
+                        type=str,
+                        help='Country you are interested in',
+                        required=True)
 
-parser.add_argument('--fires_column',
-                    type=int,
-                    help='The column in which wildfire emmissions are listed',
-                    required=True)
+    parser.add_argument('--country_column',
+                        type=int,
+                        help='Column in which countries are listed',
+                        required=True)
 
-parser.add_argument('--file_name',
-                    type=str,
-                    help='The name of the file',
-                    required=True)
+    parser.add_argument('--fires_column',
+                        type=int,
+                        help='Column in which wildfire emmissions are listed',
+                        required=True)
 
-args = parser.parse_args()
+    parser.add_argument('--file_name',
+                        type=str,
+                        help='Name of the file',
+                        required=True)
 
-fires = my_utils.get_column(args.file_name, args.country_column, args.country,
-                            result_column=args.fires_column)
-print(fires)
+    args = parser.parse_args()
+
+    fires = my_utils.get_column(args.file_name, args.country_column,
+                                args.country, result_column=args.fires_column)
+    print(fires)
+
+
+if __name__ == "__main__":
+    main()
