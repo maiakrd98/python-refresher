e59b26cc6d9c03343eb15f2b05f7a8940cfac5eb
Maia Richards-Dinger <maiakrd98@gmail.com>
2026-09-24 09:06:46 -0600

    mean tests for print_fires.py

--- test/func/test_print_fires.sh
+++ test/func/test_print_fires.sh
@@ -18,4 +18,12 @@ assert_exit_code 0
 
 # TO DO: also one for no crop residues in forest fires???
 
+run test_print_canada_fires_mean python ../../print_fires.py --file_name 'Agrofood_co2_emissions_test_file.csv' --country 'Canada' --country_column 0  --fires_column 2 --operation "mean"
+assert_in_stout "1065.0"
+assert_exit_code 0
+
+run test_print_tanzania_crop_residues_mean python ../../print_fires.py --file_name 'Agrofood_co2_emissions_test_file.csv' --country 'United Republic of Tanzania' --country_column 0  --fires_column 3 --operation "mean"
+assert_in_stout "701.7"
+assert_exit_code 0
+
 # TO DO: error testing 
